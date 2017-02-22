26728a568e13b5d05a18b96c08be9325e745df8a
github username <github email address>
2017-02-22 12:58:22 -0500

    ok

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'http://rubygems.org'
 
 gem 'activerecord', :require => 'active_record'
 gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
-gem 'pry'
+gem 'pry-byebug'
 gem 'pry-nav'
 gem 'rack-test'
 gem 'rake'
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,24 +2,46 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @new_pet = Pet.create(params[:pet])
+    if params[:pet][:owner_id].empty?
+      @new_owner = Owner.create(params[:owner])
+      @new_pet.owner_id = @new_owner.id
+    end
+    @new_pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  get '/pets/:id/edit' do
+    erb :edit
+  end
+
+  post '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(params[:owner])
+    end
+    @pet.save
+    redirect to "/pets/#{@pet.id}"
+
+    # erb :'/pets/show'
+  end
+
+
+  post '/pets/:id' do
 
     redirect to "pets/#{@pet.id}"
   end
